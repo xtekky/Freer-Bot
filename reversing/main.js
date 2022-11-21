da21f82073e1adceb20f03dca57f79cfeae01b54
t.me/xtekky <98614666+xtekky@users.noreply.github.com>
2022-11-21 14:32:28 +0000

    idk

---
+++ reversing/main.js
@@ -0,0 +1,92 @@
+var _0x2c63 = ["message", "<hr><h3>ERROR</h3>", "AES", "abs", "hide", "323768FlgZCN", "2ZXZNQn", "#msg", "ajax", "751123CfOVxJ", "257804eNFZkl", "replace", "/error", "userAgent", "split", "<hr>", "getElementById", "length", "835409FGATlr", "onkeydown", "getTime", "MD5", "show", "reverse", "654399YcmVFw", "/v4.php", "enc", '<hr><h2 class="text-danger font-weight-bold">Please update your browser or try a different one.</h2><hr>', "dvl", "keyCode", "Malformed UTF-8 data", "join", "1FDizCT", "onload", "toString", "data=", "POST", "Utf8", "9142HyysAr", "parse", "substr", "decrypt", "1390613NQfTzu", "ctrlKey", "statusText", "#loading", "25sEeaTn", "</textarea></div></div><hr>"];
+var wbpo = "//freer.es";
+var _0x335255 = _0x54c9;
+(function(_0x392d56, _0xf3c54b) {
+	var _0x644cc4 = _0x54c9;
+	while (true) {
+		try {
+			var _0x159dc9 = parseInt(_0x644cc4(340)) * parseInt(_0x644cc4(339)) + -parseInt(_0x644cc4(343)) * parseInt(_0x644cc4(318)) + parseInt(_0x644cc4(344)) + parseInt(_0x644cc4(310)) + parseInt(_0x644cc4(304)) + parseInt(_0x644cc4(324)) * parseInt(_0x644cc4(332)) + -parseInt(_0x644cc4(328));
+			if (_0x159dc9 === _0xf3c54b) break;
+			else _0x392d56.push(_0x392d56.shift());
+		} catch (_0x23bf3a) {
+			_0x392d56.push(_0x392d56.shift());
+		}
+	}
+}(_0x2c63, 481962));
+
+function _0x54c9(_0x42aa88, _0x1be0c1) {
+	return _0x54c9 = function(_0x2c6360, _0x54c9e3) {
+		_0x2c6360 = _0x2c6360 - 299;
+		var _0x4ec6c0 = _0x2c63[_0x2c6360];
+		return _0x4ec6c0;
+	}, _0x54c9(_0x42aa88, _0x1be0c1);
+}
+
+function closest(_0x2e27a6, _0x384f9c) {
+	var _0x32e167 = _0x54c9;
+	for (var _0x4710f6 = _0x384f9c[0], _0x2d06b0 = Math[_0x32e167(337)](_0x2e27a6 - _0x4710f6), _0x23f580 = 0; _0x23f580 < _0x384f9c[_0x32e167(303)]; _0x23f580++) {
+		var _0x34c6db = Math[_0x32e167(337)](_0x2e27a6 - _0x384f9c[_0x23f580]);
+		_0x34c6db < _0x2d06b0 && (_0x2d06b0 = _0x34c6db, _0x4710f6 = _0x384f9c[_0x23f580]);
+	}
+	return _0x4710f6;
+};
+
+function hsipdxdcej() {
+	var _0x3818dd = _0x54c9;
+	$(_0x3818dd(331))[_0x3818dd(338)](), $(_0x3818dd(341))[_0x3818dd(338)]();
+}
+
+function askkkdsanxc(_0x5b9380) {
+	var _0xf04372 = _0x54c9;
+	document[_0xf04372(302)]("msg").innerHTML = _0x5b9380, $(_0xf04372(341))[_0xf04372(308)]();
+}
+
+function revSt(_0x37ab47) {
+	var _0x3b4ef3 = _0x54c9;
+	return _0x37ab47[_0x3b4ef3(300)]("")[_0x3b4ef3(309)]()[_0x3b4ef3(317)]("");
+}
+
+function zxndnnndje(_0x421295) {
+	var _0x398446 = _0x54c9;
+	hsipdxdcej(), $(_0x398446(331))[_0x398446(308)](), $[_0x398446(342)]({
+		type: "POST",
+		url: "//freer.es/v4.php",
+		data: _0x421295,
+		success: function(_0x4be8a2) {
+			var _0x3a1381 = _0x398446;
+			hsipdxdcej();
+			try {
+				var _0x218bef = JSON[_0x3a1381(325)](CryptoJS[_0x3a1381(336)][_0x3a1381(327)](revSt("}" + _0x4be8a2 + "{")[_0x3a1381(345)](/58290/g, ":")[_0x3a1381(345)](/90190/g, '"').replace(/56370/g, ","), CryptoJS[_0x3a1381(307)](navigator[_0x3a1381(299)]).toString()[_0x3a1381(326)](5, 10), {
+					format: CryptoJSAesJson
+				})[_0x3a1381(320)](CryptoJS[_0x3a1381(312)][_0x3a1381(323)]));
+				_0x218bef && _0x218bef[0] && new Function(atob(_0x218bef[0]))();
+			} catch (_0x34ad79) {
+				if (_0x34ad79[_0x3a1381(334)] == _0x3a1381(316)) askkkdsanxc(_0x3a1381(313));
+				else {
+					var _0x1328f3 = JSON.stringify({
+						error: _0x34ad79[_0x3a1381(334)],
+						post: _0x421295,
+						response: _0x4be8a2
+					});
+					askkkdsanxc('<hr><h3>Please try again later</h3><br><p><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseerror" aria-expanded="false" aria-controls="collapseerror">Error Details</button></p><div class="collapse" id="collapseerror"><div class="card card-body"><textarea id="errorlog">' + _0x1328f3 + _0x3a1381(333)), $.post(wbpo + _0x3a1381(346), _0x3a1381(321) + btoa(_0x1328f3));
+				}
+			}
+		},
+		error: function(_0x3eee08) {
+			var _0x4de989 = _0x398446;
+			hsipdxdcej(), askkkdsanxc(_0x4de989(335) + _0x3eee08[_0x4de989(330)] + _0x4de989(301));
+		}
+	});
+}
+
+setTimeout(function() {
+	location.reload();
+}, 36e5);
+
+var xt = setInterval(function() {
+	var _0x3449c0 = _0x335255,
+		_0x20c6e0 = (new Date)[_0x3449c0(306)]();
+	debugger;
+	(new Date)[_0x3449c0(306)]() - _0x20c6e0 > 100 && (window[_0x3449c0(314)] = 1, clearInterval(xt));
+}, 100);
+console.log(_0x54c9(322));
